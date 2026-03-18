09629b9e6c260f84b735b9867a555634f24405d1
kateryna-ponomarenko <kateryna.ponomarenko.25@kse.org.ua>
2026-03-18 23:40:49 +0200

    Update Postfix.cs
    finished postfix

--- Postfix.cs
+++ Postfix.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace calculator;
 
 public class Postfix
@@ -5,32 +7,109 @@ public class Postfix
     private Tokens _tokens;
     private string _example;
     
+    
     public Postfix(Tokens tokens, string example)
     {
         _tokens = tokens;
         _example = example;
     }
     
-    private Stack _stack = new Stack();
-    private Queue _queue = new Queue();
-
-    public void ShuntingYardAlgorithm()
+    public int GetPriority(string symbol)
     {
-        while (_tokens.DivisionForTokens(_example).Count() > 0)
+        if (symbol == "+" || symbol == "-")
         {
-            string token =_tokens.DivisionForTokens(_example).Dequeue();
+            return 2;
+        }
+        else if (symbol == "*" || symbol == "/")
+        {
+            return 3;
+        }
+        else if (symbol == "^")
+        {
+            return 4;
+        }
+        else if (symbol == "(")
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static bool IsOperator(string symbol)
+    {
+        return symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/";
+    }
+    
+    
+    public Queue ShuntingYardAlgorithm()
+    {
+        
+        Stack stack = new Stack();
+        Queue queue = new Queue();
+        
+        Queue inputTokens = _tokens.DivisionForTokens(_example);
+        
+        string previousToken = "";
+        
+        
+        while (inputTokens.Count() > 0)
+        {
+            string token = inputTokens.Dequeue();
+            
             // tryParse перевіряє, чи є рядок числом, враховуючи багатозначність та роздільники (крапки/коми).
             // out _ дозволяє перевірити тип, не зберігаючи результат у змінну
+            // if num
             if (double.TryParse(token, out _))
             {
-                _queue.Enqueue(token);
+                queue.Enqueue(token);
             }
-
-            else if(_tokens.operators.Contains(token))
+            
+            // if operator
+            if (IsOperator(token))
             {
-                
+                if (stack.Count == 0 || stack.Peek() == "("){stack.Push(token);}
+                else if (GetPriority(stack.Peek()) < GetPriority(token) && stack.Count > 0)
+                {
+                    stack.Push(token);
+                }
+                else if (GetPriority(stack.Peek()) >= GetPriority(token))
+                {
+                    while (stack.Count > 0 && GetPriority(stack.Peek()) >= GetPriority(token) && stack.Peek() != "(")
+                    {
+                        queue.Enqueue(stack.Pop());
+                    }
+                    stack.Push(token);
+                }
             }
+            //if )
+            if (token == "(")
+            {
+                if (double.TryParse(previousToken, out _) && !string.IsNullOrEmpty(previousToken))
+                {
+                    throw new Exception("dont have operator between num and ( ");
+                }
+                else
+                {
+                    stack.Push(token);
+                }
+            }
+            //if (
+            if (token == ")")
+            {
+                while (stack.Count > 0 && stack.Peek() != "(")
+                {
+                    queue.Enqueue(stack.Pop());
+                }
+                stack.Pop();
+            }
+            
+            previousToken = token;
         }
         
+        while (stack.Count > 0)
+        {
+            queue.Enqueue(stack.Pop());
+        }
+        return queue;
     }
 }
