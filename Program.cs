9f8e0bc6308d6f26f5ad0e5a29207f97249feb31
kateryna-ponomarenko <kateryna.ponomarenko.25@kse.org.ua>
2026-03-18 23:41:18 +0200

    Update Program.cs
    program

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿namespace calculator;
+using System;
+
+namespace calculator;
 
 class Program
 {
     static void Main(string[] args)
     {
         Tokens tokens = new Tokens();
-        tokens.DivisionForTokens("9 + 7 ");
+        // tokens.DivisionForTokens("9 + 7 ");
+
+        Postfix postfix = new Postfix(tokens,"7*8+(5 + 9) ");
         
-        
-        
-        
-        
-        
-        
-        
+        Queue queue = postfix.ShuntingYardAlgorithm(); //??
+        CalculateResult calculateResult = new CalculateResult(queue);
+        string res = calculateResult.Result();
+        Console.WriteLine(res);
+
+
+
+
+
     }
 }
